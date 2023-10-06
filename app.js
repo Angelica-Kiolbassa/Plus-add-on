63ddd5f58ac0e36640582e20da811751ce3303cd
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-10-06 11:51:10 -0500

    styled app
    added CSS to style app design

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ function showSelectedCity(event) {
 let citySelect = document.querySelector("#city");
 
 citySelect.addEventListener("change", showSelectedCity);
+
+function showSelectedCityTwo(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+let cityTwoSelect = document.querySelector("#cityTwo");
+
+cityTwoSelect.addEventListener("change", showSelectedCity);
