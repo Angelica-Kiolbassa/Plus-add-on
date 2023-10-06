6e70e5fa57cf74776589ee1e9a73ed4fbb6909b7
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-10-06 15:20:52 -0500

    updated content to real time
    updated city one and two displays to real time with moment.

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+// city one results
+let cityOneElement = document.querySelector("#city-one");
+let cityOneDateElement = cityOneElement.querySelector(".date");
+let cityOneTimeElement = cityOneElement.querySelector(".time");
+let cityOneTime = moment().tz("America/Los_Angeles");
+
+cityOneDateElement.innerHTML = cityOneTime.format("MMMM Do YYYY");
+cityOneTimeElement.innerHTML = cityOneTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+// city two results
+let cityTwoElement = document.querySelector("#city-two");
+let cityTwoDateElement = cityTwoElement.querySelector(".date");
+let cityTwoTimeElement = cityTwoElement.querySelector(".time");
+let cityTwoTime = moment().tz("Asia/Tokyo");
+
+cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM Do YYYY");
+cityTwoTimeElement.innerHTML = cityTwoTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+// drop down functions
 function showSelectedCity(event) {
   if (event.target.value.length > 0) {
     let currentTime = moment()
