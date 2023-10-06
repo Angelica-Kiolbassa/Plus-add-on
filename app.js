8a6dcd2b8c896ceaec7dcd947397ca660b0b3f96
Angelica K <129087230+Angelica-Kiolbassa@users.noreply.github.com>
2023-10-06 16:50:45 -0500

    working on function for dropdown
    working on function for dropdown/on change.

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
-// city one results
-let cityOneElement = document.querySelector("#city-one");
-let cityOneDateElement = cityOneElement.querySelector(".date");
-let cityOneTimeElement = cityOneElement.querySelector(".time");
-let cityOneTime = moment().tz("America/Los_Angeles");
+function updateTime() {
+  // city one results
+  let cityOneElement = document.querySelector("#city-one");
+  if (cityOneElement) {
+    let cityOneDateElement = cityOneElement.querySelector(".date");
+    let cityOneTimeElement = cityOneElement.querySelector(".time");
+    let cityOneTime = moment().tz("America/Los_Angeles");
 
-cityOneDateElement.innerHTML = cityOneTime.format("MMMM Do YYYY");
-cityOneTimeElement.innerHTML = cityOneTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+    cityOneDateElement.innerHTML = cityOneTime.format("MMMM Do YYYY");
+    cityOneTimeElement.innerHTML = cityOneTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-// city two results
-let cityTwoElement = document.querySelector("#city-two");
-let cityTwoDateElement = cityTwoElement.querySelector(".date");
-let cityTwoTimeElement = cityTwoElement.querySelector(".time");
-let cityTwoTime = moment().tz("Asia/Tokyo");
+  // city two results
+  let cityTwoElement = document.querySelector("#city-two");
+  if (cityTwoElement) {
+    let cityTwoDateElement = cityTwoElement.querySelector(".date");
+    let cityTwoTimeElement = cityTwoElement.querySelector(".time");
+    let cityTwoTime = moment().tz("Asia/Tokyo");
 
-cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM Do YYYY");
-cityTwoTimeElement.innerHTML = cityTwoTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+    cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM Do YYYY");
+    cityTwoTimeElement.innerHTML = cityTwoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
 
 // drop down functions
-function showSelectedCity(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY h:mm A");
+// function showSelectedCityOne(event) {
+//   if (event.target.value.length > 0) {
+//     let currentTime = moment()
+//       .tz(event.target.value)
+//       .format("dddd, MMMM D, YYYY h:mm A");
 
-    alert(`It is ${currentTime} in ${event.target.value}`);
-  }
+//     alert(`It is ${currentTime} in ${event.target.value}`);
+//   }
+// }
+// let citySelect = document.querySelector("#cityOne");
+
+// citySelect.addEventListener("change", showSelectedCityOne);
+
+// function showSelectedCityTwo(event) {
+//   if (event.target.value.length > 0) {
+//     let currentTime = moment()
+//       .tz(event.target.value)
+//       .format("dddd, MMMM D, YYYY h:mm A");
+
+//     alert(`It is ${currentTime} in ${event.target.value}`);
+//   }
+// }
+// let cityTwoSelect = document.querySelector("#cityTwo");
+
+// cityTwoSelect.addEventListener("change", showSelectedCityTwo);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let firstCityElement = document.querySelector("#city-one");
+  firstCityElement.innerHTML = `
+  <div id="city-one">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
-let citySelect = document.querySelector("#city");
 
-citySelect.addEventListener("change", showSelectedCity);
+updateTime();
+setInterval(updateTime, 1000);
 
-function showSelectedCityTwo(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY h:mm A");
-
-    alert(`It is ${currentTime} in ${event.target.value}`);
-  }
-}
-let cityTwoSelect = document.querySelector("#cityTwo");
-
-cityTwoSelect.addEventListener("change", showSelectedCity);
+let firstCitySelected = document.querySelector("#cityOne");
+firstCitySelected.addEventListener("change", updateCity);
